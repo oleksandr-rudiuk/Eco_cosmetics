cb2c1ca457cbd255dbe330077a12c063209afd4c
Oleksandr Rudiuk <Alvlru629@gmail.com>
2023-10-27 11:54:31 +0300

    eco

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const formfieldElement = document.getElementById('formfield');
+const nameInputElement = document.getElementById('formfield-name');
+const emailInputElement = document.getElementById('formfield-email');
+const messageInputElement = document.getElementById('formfield-massage');
+const phoneInputElement = document.getElementById('formfield-phone');
+
+formfieldElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  nameInputElement.value = '';
+  emailInputElement.value = '';
+  messageInputElement.value = '';
+  phoneInputElement.value = '';
+
+  window.alert('Sent successfully!');
+});
